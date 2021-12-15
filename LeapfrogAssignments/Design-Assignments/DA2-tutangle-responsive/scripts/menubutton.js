bd46fa3c5a1bdb55037c07b45beb10443e629e4c
Shreedhar73 <shreedharpandey000@gmail.com>
2021-12-15 15:44:29 +0545

    Tutangle Responsive

---
+++ LeapfrogAssignments/Design-Assignments/DA2-tutangle-responsive/scripts/menubutton.js
@@ -0,0 +1,10 @@
+
+        function displayNav(){
+            if (document.getElementById('menubutton').style.display == "block"){
+                document.getElementById('menubutton').style.display = "none";
+            } else {
+                document.getElementById('menubutton').style.display = "block";
+
+            }
+        }
+    
