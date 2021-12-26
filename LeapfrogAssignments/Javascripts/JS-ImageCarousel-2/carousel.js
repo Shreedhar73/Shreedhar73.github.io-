17467fcc81f850b81967dc6387559b689ae066b8
Shreedhar73 <shreedharpandey000@gmail.com>
2021-12-26 21:04:11 +0545

    assignment 2

--- LeapfrogAssignments/Javascripts/JS-ImageCarousel-2/carousel.js
+++ LeapfrogAssignments/Javascripts/JS-ImageCarousel-2/carousel.js
@@ -7,17 +7,34 @@ var imageWrapper = document.querySelector('.carousel-image-wrapper');
 var UNIT = 'px';
 var INITIALPOINT = "0px";
 var isMoving = false;
+transitionTime = prompt("Enter transition time");
+if((transitionTime == 0 ) || (transitionTime == undefined)){
 
+    alert("Relod the page and enter transition time");
+    location.reload();
+}
 
-//creating previous next button
+var holdTime = prompt("Enter hold time in seconds");
 
+if((holdTime == 0 ) || (holdTime == undefined)){
+    alert("Relod the page and enter hold time");
+    location.reload();
+}
+
+//previous button
+function prevBtn(){
 
 let prevButton = document.createElement("button");
 prevButton.innerHTML = "<";
 prevButton.id = "previous";
 prevButton.style.top = "250px";
 document.getElementById("carousel").appendChild(prevButton);
+}
+prevBtn();
 
+
+//next button
+function nxtBtn(){
 let nextButton = document.createElement("button");
 nextButton.innerHTML = ">";
 nextButton.id = "next";
@@ -25,10 +42,11 @@ nextButton.style.position = "absolute";
 nextButton.style.top = "255px";
 nextButton.style.left = "94%";
 document.getElementById("carousel").appendChild(nextButton);
+}
+nxtBtn();
 
-
-
-
+//radio button for indicator
+function radioBtn(){
 for(i = 0; i<sliderImages.length;i++){
     var radio = document.createElement("input");
     radio.type = "radio";
@@ -39,34 +57,42 @@ for(i = 0; i<sliderImages.length;i++){
     document.getElementById("pointer").appendChild(radio);
     
     }
+}
 
 
 
-
-
+radioBtn();
 
 
 //shift right
 function moveRight() {
     currentIndex++;
-    isMoving = true;
+    
     if (currentIndex < sliderImages.length) {
+        isMoving = true;
         window.requestAnimationFrame(imageSliderRight);
     } else if (currentIndex == sliderImages.length) {
         currentIndex = 0;
         imageWrapper.style.left = MAXWIDTH + UNIT;
         imageSliderRight();
+        isMoving = false;
     } 
 }
 
 //shift left
 function moveLeft() {
+    
+    
     currentIndex--;
+   
+
     
     if (currentIndex < 0) {
         isMoving = true;
         window.requestAnimationFrame(firstToEnd);
+        
     } else if (currentIndex >= 0) {
+        isMoving = true;
         window.requestAnimationFrame(imageSliderLeft);
     }
 }
@@ -78,23 +104,20 @@ function moveLeft() {
 
 
 function imageSliderRight() {
-    var leftPx = parseInt(window.getComputedStyle(imageWrapper).left) - 15;
+    var leftPx = parseInt(window.getComputedStyle(imageWrapper).left) - (transitionTime);
     imageWrapper.style.left = leftPx + UNIT;
     
-    if (parseInt(leftPx) != -(MAXWIDTH * currentIndex)) {
-        if (parseInt(leftPx) < -(MAXWIDTH * currentIndex)) {
+    if (parseInt(leftPx) < -(MAXWIDTH * currentIndex)) {
+        
             isMoving = false;
             imageWrapper.style.left = -(MAXWIDTH * currentIndex) + UNIT;
         } else {
             window.requestAnimationFrame(imageSliderRight);
         }
+        indicator("right");
+        
     }
-    indicator("right");
-}
-
-
-
-
+    
 function indicator(status) {
     for (var i = 1; i <= sliderImages.length-1; i++) {
         if (window.getComputedStyle(imageWrapper).left == -(MAXWIDTH * (sliderImages.length)) + UNIT || imageWrapper.style.left == INITIALPOINT) {
@@ -119,26 +142,28 @@ function indicator(status) {
 
 
 function imageSliderLeft() {
-    indicator("left");
-    var rightPx = parseInt(imageWrapper.style.left) + 15;
+    
+    var rightPx = parseInt(imageWrapper.style.left) + (transitionTime);
     imageWrapper.style.left = rightPx + UNIT;
-    if (rightPx != -(MAXWIDTH * currentIndex)) {
-        if (parseInt(rightPx) > -(MAXWIDTH * currentIndex)) {
+    if (rightPx > -(MAXWIDTH * currentIndex)) {
+        
             isMoving = false;
             imageWrapper.style.left = -(MAXWIDTH * currentIndex) + UNIT;
         } else {
             window.requestAnimationFrame(imageSliderLeft);
         }
+
+        indicator("left");
     }
 
-}
+
 
 function firstToEnd() {
     currentIndex = sliderImages.length - 1;
     document.querySelector(`#radio${sliderImages.length - 1}`).checked = true;
     if (window.getComputedStyle(imageWrapper).left == INITIALPOINT || imageWrapper.style.left == INITIALPOINT) {
         function end() {
-            var rightPx = (parseInt(window.getComputedStyle(imageWrapper).left) - 50);
+            var rightPx = (parseInt(window.getComputedStyle(imageWrapper).left) - (100));
             imageWrapper.style.left = rightPx + UNIT;
             if (rightPx != -(MAXWIDTH*currentIndex)) {
                 window.requestAnimationFrame(end);
@@ -151,6 +176,9 @@ function firstToEnd() {
     }
 }
 
+setInterval(()=>{
+    moveRight();
+},holdTime+"000")
 
 document.getElementById('next').onclick = function () {
     if (isMoving == false) {
@@ -158,12 +186,6 @@ document.getElementById('next').onclick = function () {
 
     }
 
-}
-
-document.getElementById('previous').onclick = function () {
-    if (isMoving == false) {
-        moveLeft();
-    }
 }
 
 radioButton = document.querySelectorAll('.radio');
